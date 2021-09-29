d435c73e64967622b18f01490440ce272088b60a
Manu00 <manuelschmidt785@gmail.com>
2021-09-29 20:59:00 +0200

    fixen/added data retrival
    plus some smal adjustments

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from logging import PlaceHolder, shutdown
 from flask import Flask, redirect, url_for, request, render_template
-import datetime, os, sys, uuid
+import datetime
+import os
+import sys
+import uuid
 import db
 import json
 
@@ -10,55 +13,73 @@ app = Flask(__name__)
 result = 0
 database = db.db()
 
-@app.route("/", methods = ["POST", "GET"])
+
+@app.route("/", methods=["POST", "GET"])
 def index():
     listOfItems = database.getDistinct()
     items = []
     checked = []
     for item in listOfItems:
-      items.append(item[0])
+        items.append(item[0])
     #tests()
     if request.method == "GET":
-        return render_template("index.html", checked = checked, items = items)
+        return render_template("index.html", checked=checked, items=items, data = data)
     if request.method == "POST":
         form_data = request.form
         if form_data == "1h":
-          pass
+            pass
         elif form_data == "2h":
-          pass
+            pass
         elif form_data == "4h":
-          pass
+            pass
         elif form_data == "8h":
-          pass
+            pass
         else:
-          for i in form_data.values():
-            checked.append(i)
+            for i in form_data.values():
+                checked.append(i)
         #form_data = database.get_distinct()
         print(form_data)
         print(checked)
-        return render_template("index.html", checked = checked, items = items)
+        return render_template("index.html", checked=checked, items=items, data = data)
 
-@app.route("/api", methods = ["POST", "GET"])
+
+@app.route("/api", methods=["POST", "GET"])
 def api():
     if request.method == "POST":
-      item = request.json.get("name")
-      database.storeItem(datetime.datetime.now(), item, uuid.uuid4())
-      print(item)
-      return "ok"
+        item = request.json.get("name")
+        database.storeItem(datetime.datetime.now(), item, uuid.uuid4())
+        print(item)
+        return "ok"
     else:
-      return "ok"
+        return "ok"
+
 
 def getData(timescale, items):
-  dataset = {}
-  startTime = datetime.datetime.now() - datetime.timedelta(0,0,0,0,0,timescale,0)
-  endTime = datetime.datetime.now()
-  for item in items:
-    dataset[item] = database.getItem(startTime, endTime, item)
-  print(dataset)
-  return dataset
+    test = {
+      "items": [
+
+      ]
+    }
+    startTime = datetime.datetime.now() - datetime.timedelta(0, 0, 0, 0, 0, timescale, 0)
+    endTime = datetime.datetime.now()
+    for item in items.keys():
+        test["items"].append({"name": item, "size": database.getItemcount(item), "timestamps": database.getItem(startTime, endTime, item)})
+    result = json.dumps(test, indent = 4)
+    return result
+
 
 def tests():
     #database.storeItem(datetime.time, "Bier", 1)
-    database.getItem(datetime.datetime.now() - datetime.timedelta(hours=8), datetime.datetime.now(), "wasser")
-    print(datetime.datetime.now() - datetime.timedelta(hours=2))
-    print(datetime.datetime.now())
+    getData(48, {"wasser": "wasser", "weizen": "weizen"})
+    #database.getItem(datetime.datetime.now() - datetime.timedelta(hours=24), datetime.datetime.now(), "wasser")
+
+
+class itemCollection:
+  size = 0
+  name = ""
+  timestamps = []
+
+  def __init__(self, size, name, timestamps) -> None:
+      size = size
+      name = name
+      timestamps = timestamps
--- db.py
+++ db.py
@@ -2,54 +2,69 @@ import os
 import sys
 import sqlite3
 
+
 class db:
 
-    #check for existing database, create one if necessary
+    # check for existing database, create one if necessary
     def __init__(self):
         if os.path.exists("beercounter.db"):
             print("using existing database")
-            connection = sqlite3.connect("beercounter.db", check_same_thread=False)
+            connection = sqlite3.connect(
+                "beercounter.db", check_same_thread=False)
         else:
             print("creating new database")
-            connection = sqlite3.connect("beercounter.db", check_same_thread=False)
+            connection = sqlite3.connect(
+                "beercounter.db", check_same_thread=False)
             sql = "CREATE TABLE items(" \
                 "time TEXT, " \
                 "name TEXT, " \
                 "id TEXT PRIMARY KEY)"
             cursor.execute(sql)
             connection.commit()
-    
+
     def closeDb(self):
         connection.commit()
         connection.close()
-    
+
     def storeItem(self, time, name, id):
         #sql = "INSERT INTO items VALUES(" + "'" + time + "', " + "'" + name + "', " + "'" + id + "')"
         #sql = "INSERT INTO items VALUES({time}, {name}, {id})"
-        cursor.execute("INSERT INTO items VALUES(?,?,?)", (str(time), str(name), str(id)))
+        cursor.execute("INSERT INTO items VALUES(?,?,?)",
+                       (str(time), str(name), str(id)))
         print("Storing new data:")
-        #print(time, name, id)
-        #print(sql)
-        #cursor.execute(sql)
+        print(time, name, id)
+        # print(sql)
+        # cursor.execute(sql)
         connection.commit()
 
-    
     def getItemOLD(self):
         sql = "SELECT * FROM items"
         cursor.execute(sql)
         for dsatz in cursor:
             print(dsatz[0], dsatz[1])
-    
+
     def getItem(self, startTime, endTime, item):
         cursor.execute("SELECT time FROM items WHERE strftime('%Y-%m-%d %H:%M:%S.%f', time) BETWEEN ? AND ? AND name = ?", (str(startTime), str(endTime), str(item)))
-        print(cursor.fetchall())
-        
-    
+        rows = cursor.fetchall()
+        result = []
+        for row in rows:
+            result.append(row[0])
+        return(result)
+
+    def getItemcount(self, item):
+        cursor.execute("SELECT count(*) FROM items WHERE name = ? AND name = ?", (str(item), str(item)))
+        temp = cursor.fetchall()
+        print("database has ")
+        print(temp[0][0])
+        print(" of item")
+        print(item)
+        return(int(temp[0][0]))
+
     def getDistinct(self):
         sql = "SELECT DISTINCT name FROM items"
         cursor.execute(sql)
         return cursor.fetchall()
 
-        
+
 connection = sqlite3.connect("beercounter.db", check_same_thread=False)
 cursor = connection.cursor()
